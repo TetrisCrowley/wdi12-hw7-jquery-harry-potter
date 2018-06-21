6a84ece2d57166c2dcf9321a05389cb8730f182d
Steve <sbrennison@gmail.com>
2018-06-21 18:20:01 -0500

    Added more elements. Power grows and whatever

--- js/app.js
+++ js/app.js
@@ -16,3 +16,33 @@
 
  const $h1 = $('<h1>Hogwarts</h1>');
  $container.append($h1);
+
+ const $h2 = $('<h2>Steve</h2>');
+ $container.append($h2);
+
+ const $h3 = $('<h3>Slytherin</h3>');
+ $container.append($h3);
+
+ const $h4 = $('<h4>Kurt Russell</h4>');
+ $h4.addClass('Badass');
+ $container.append($h4);
+
+ const $h4_2 = $('<h4>Wand</h4>');
+ $h4_2.addClass('Hawthorn with Dragon Heartstring Core');
+ $container.append($h4_2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
